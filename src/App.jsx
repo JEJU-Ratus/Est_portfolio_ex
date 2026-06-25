e8e1dc07f59626c3128a4cd7a7f8e6318ff3ccbb
jema <smh1141@naver.com>
2026-06-25 17:08:23 +0900

    fix : 이미지 경로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,17 @@ import "./App.css";
 import Figure from "./components/Figure";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
-import left from "./images/left.png";
-import right from "./images/right.png";
 
 function App() {
   const works = [
-    { id: 0, title: "work1", url: "./src/images/img-1.jpg", desc: "설명1" },
-    { id: 1, title: "work2", url: "./src/images/img-2.jpg", desc: "설명2" },
-    { id: 2, title: "work3", url: "./src/images/img-3.jpg", desc: "설명3" },
-    { id: 3, title: "work4", url: "./src/images/img-4.jpg", desc: "설명4" },
-    { id: 4, title: "work5", url: "./src/images/img-1.jpg", desc: "설명5" },
-    { id: 5, title: "work6", url: "./src/images/img-2.jpg", desc: "설명6" },
-    { id: 6, title: "work7", url: "./src/images/img-3.jpg", desc: "설명7" },
-    { id: 7, title: "work8", url: "./src/images/img-4.jpg", desc: "설명8" },
+    { id: 0, title: "work1", url: "/images/img-1.jpg", desc: "설명1" },
+    { id: 1, title: "work2", url: "/images/img-2.jpg", desc: "설명2" },
+    { id: 2, title: "work3", url: "/images/img-3.jpg", desc: "설명3" },
+    { id: 3, title: "work4", url: "/images/img-4.jpg", desc: "설명4" },
+    { id: 4, title: "work5", url: "/images/img-1.jpg", desc: "설명5" },
+    { id: 5, title: "work6", url: "/images/img-2.jpg", desc: "설명6" },
+    { id: 6, title: "work7", url: "/images/img-3.jpg", desc: "설명7" },
+    { id: 7, title: "work8", url: "/images/img-4.jpg", desc: "설명8" },
   ];
   const [id, setId] = useState(0);
   const currentIndex = works.findIndex(w => w.id === id);
@@ -37,13 +35,13 @@ function App() {
       <Nav data={works} activeId={id} onChangeMode={setId} />
       <div className="carousel">
         <button type="button" className="carousel-button prev-button" onClick={handlePrev}>
-          <img src={left} alt="이전" />
+          <img src="/images/left.png" alt="이전" />
         </button>
 
         {work && <Figure data={work} />}
 
         <button type="button" className="carousel-button next-button" onClick={handleNext}>
-          <img src={right} alt="다음" />
+          <img src="/images/right.png" alt="다음" />
         </button>
       </div>
     </>
